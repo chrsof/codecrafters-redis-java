3493667760f26ccbb7f79687d3d52c5b2ed890dc
cs <casofianou@gmail.com>
2024-12-31 18:36:41 +0100

    Respond to PING #RG2

--- test_redis.sh
+++ test_redis.sh
@@ -6,11 +6,23 @@ function bind_port() {
     printf 'Connection to port 6379 refused\nTest Failed'
     exit 1
   fi
-  printf 'Connection to port 6379\nTest Passed\n'
+  printf 'Connection to port 6379 succeeded\nTest Passed\n'
+}
+
+function respond_ping() {
+  printf 'Running test for Stage #RG2 (Respond to PING)\n'
+  response=$(echo "PING\r\n" | nc localhost 6379)
+  if [[ "$response" != "+PONG"* ]]; then
+    printf 'Expected +PONG received %s\nTest Failed' "$response"
+    exit 1
+  fi
+  printf 'Received +PONG\nTest Passed\n'
 }
 
 function test() {
   bind_port
+  printf '\n'
+  respond_ping
 }
 
 if [ $# -eq 0 ]; then
